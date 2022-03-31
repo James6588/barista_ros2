1bc0a635c50cbb5d5b496dfd3ba47b34ae4124be
rodrigo55 <roalgoal@gmail.com>
2022-03-31 17:21:18 +0200

    added world and models

--- barista_description/launch/spawn_gazebo.launch.py
+++ barista_description/launch/spawn_gazebo.launch.py
@@ -1,9 +1,9 @@
-import launch
 from launch.substitutions import Command, LaunchConfiguration, FindExecutable, PathJoinSubstitution
-import launch_ros
-import os
-
 from launch_ros.substitutions import FindPackageShare
+import launch_ros
+import launch
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
@@ -19,56 +19,23 @@ def generate_launch_description():
 
     robot_description = {"robot_description": robot_description_content}
 
+    barista_description = get_package_share_directory('barista_description')
     pkg_share = launch_ros.substitutions.FindPackageShare(package='barista_description').find('barista_description')
     default_model_path = os.path.join(pkg_share, 'src/description/barista.urdf')
-    # default_rviz_config_path = os.path.join(pkg_share, 'rviz/diffbot.rviz')
-    default_ros2_control_config_path = os.path.join(pkg_share, 'config/rb1_controller.yaml')
-
-    # control_node = launch_ros.actions.Node(
-    #     package="controller_manager",
-    #     executable="ros2_control_node",
-    #     parameters=[robot_description, default_ros2_control_config_path],
-    #     output={
-    #         "stdout": "screen",
-    #         "stderr": "screen",
-    #     },
-    # )
-
-    # joint_state_broadcaster_spawner = launch_ros.actions.Node(
-    #     package="controller_manager",
-    #     executable="spawner.py",
-    #     arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
-    # )
-
-    # robot_controller_spawner = launch_ros.actions.Node(
-    #     package="controller_manager",
-    #     executable="spawner.py",
-    #     arguments=["rb1_base_controller", "-c", "/controller_manager"],
-    # )
 
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}],
-        # remappings=[
-        #     ("/rb1_base_controller/cmd_vel_unstamped", "/cmd_vel"),
-        # ],
+
     )
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
-        # condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
 
-    # rviz_node = launch_ros.actions.Node(
-    #     package='rviz2',
-    #     executable='rviz2',
-    #     name='rviz2',
-    #     output='screen',
-    #     arguments=['-d', LaunchConfiguration('rvizconfig')],
-    # )
     spawn_entity = launch_ros.actions.Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
@@ -77,18 +44,21 @@ def generate_launch_description():
     )
 
     return launch.LaunchDescription([
+
+        launch.actions.DeclareLaunchArgument('worlds',
+            default_value=[os.path.join(barista_description, 'worlds', 'tc_coffeeshop.world'), ''],
+            description='SDF world file',),
+
         launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
                                             description='Flag to enable joint_state_publisher_gui'),
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
                                             description='Absolute path to robot urdf file'),
-        # launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-        #                                     description='Absolute path to rviz config file'),
-        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'], output='screen'),
+        
+
+        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', 'worlds'], output='screen'),
+
         spawn_entity,
         joint_state_publisher_node,
         robot_state_publisher_node,
-        # control_node,
-        # joint_state_broadcaster_spawner,
-        # robot_controller_spawner,
-        # rviz_node
+
     ])

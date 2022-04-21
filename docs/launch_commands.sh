bed1b5c3ed9f2db2e4b0b628b73d52a2c4a64c87
RDaneelOlivaw <duckfrost@gmail.com>
2022-04-21 18:37:15 +0200

    Added working map show

--- docs/launch_commands.sh
+++ docs/launch_commands.sh
@@ -21,3 +21,32 @@ ros2 launch rmf_demos_gz hotel.launch.xml
 
 
 export GAZEBO_MODEL_PATH=/home/tgrip/TheConstruct/ros_2_playground/coffee_shop_ws/build/rmf_tc/maps/simple/models
+
+################
+# EDIT COMMANDS
+################
+
+colcon build --packages-select barista_rmf_gazebo
+
+traffic-editor
+
+
+#############################
+# DEBUGGING TESTS
+#############################
+# We launch only the simulation ( generated in build time using teh rmf_config/simple1_big.building.yaml)
+ros2 launch barista_rmf_gazebo main.launch.xml
+
+# >>>> Sale esto:
+# [slotcar_barista_1]: Unable to determine the current level_name for robot [barista_1].
+# Kindly ensure the building_map_server is running. 
+# The RobotState message for this robot will not be published.
+
+ros2 launch barista_rmf_gazebo start_rmf.launch.xml
+
+
+################
+# Tests to rebbot whats going on
+################
+
+ros2 launch rmf_demos_gz hotel_tests.launch.xml
